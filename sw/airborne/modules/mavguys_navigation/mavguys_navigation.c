c6607289d26a1212d081a4f858fdfe79a8563aa7
Barry <barryvanleeuwen_95@hotmail.com>
2020-03-24 16:40:48 +0100

    adding comments

--- sw/airborne/modules/mavguys_navigation/mavguys_navigation.c
+++ sw/airborne/modules/mavguys_navigation/mavguys_navigation.c
@@ -26,6 +26,7 @@
  * define which filter to use. 
  */
 
+// STUDENT ADDITIONS: including necessary modules for the nav to run
 #include "modules/mavguys_navigation/mavguys_navigation.h"
 #include "firmwares/rotorcraft/guidance/guidance_h.h"
 #include "generated/airframe.h"
@@ -34,6 +35,7 @@
 #include <stdio.h>
 #include <time.h>
 
+// STUDENT ADDITIONS: defining variables, functions and states
 #define ORANGE_AVOIDER_VERBOSE TRUE
 
 #define PRINT(string,...) fprintf(stderr, "[floor_color_detector->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
@@ -57,8 +59,8 @@ enum navigation_state_t {
 // define settings
 float oag_color_count_frac = 0.18f;       // obstacle detection threshold as a fraction of total of image
 float oag_floor_count_frac = 0.05f;       // floor detection threshold as a fraction of total of image
-float oag_max_speed = 1.2f;               // max flight speed [m/s]
-float oag_heading_rate = RadOfDeg(50.f);  // heading change setpoint for avoidance [rad/s]
+float oag_max_speed = 1.3f;               // max flight speed [m/s]
+float oag_heading_rate = RadOfDeg(30.f);  // heading change setpoint for avoidance [rad/s]
 float leftorright = 0.0f;                   //safe, links of rights
 
 // define and initialise global variables
@@ -87,7 +89,7 @@ static void color_detection_cb(uint8_t __attribute__((unused)) sender_id,
 */
 //int16_t leftorright = 1; //the value from 0,1,2 for safe, right and left from vision code
 
-
+// STUDENT ADDITIONS:Communication set-up between vision and navigation
 #ifndef FLOOR_VISUAL_DETECTION_ID
 #error This module requires two color filters, as such you have to define FLOOR_VISUAL_DETECTION_ID to the orange filter
 #error Please define FLOOR_VISUAL_DETECTION_ID to be COLOR_OBJECT_DETECTION1_ID or COLOR_OBJECT_DETECTION2_ID in your airframe
@@ -98,20 +100,20 @@ static void floor_detection_cb(uint8_t __attribute__((unused)) sender_id,
                                int16_t __attribute__((unused)) pixel_width, int16_t __attribute__((unused)) pixel_height,
                                int32_t quality, int16_t __attribute__((unused)) extra)
 {
-  leftorright = quality;
+  leftorright = quality; //STUDENT ADDITIONS: command function (safe, right or left)
   floor_centroid = pixel_y;
 }
 
 /*
  * Initialisation function
  */
-void mavguys_navigation_init(void)
+void mavguys_navigation_init(void) // STUDENT ADDITIONS: initialising the c-file
 {
   // Initialise random values
   srand(time(NULL));
-  chooseRandomIncrementAvoidance();
+  chooseRandomIncrementAvoidance(); //STUDENT ADDITIONS: function is used in the beginning
 
-  // bind our colorfilter callbacks to receive the color filter outputs
+  //STUDENT ADDITIONS: bind our vision program callbacks to receive the vision outputs
   //AbiBindMsgVISUAL_DETECTION();//FLOOR_COLOR_DETECTOR_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
   AbiBindMsgVISUAL_DETECTION(FLOOR_VISUAL_DETECTION_ID, &floor_detection_ev, floor_detection_cb);
 }
@@ -133,26 +135,29 @@ void mavguys_navigation_periodic(void)
   int32_t floor_count_threshold = oag_floor_count_frac * front_camera.output_size.w * front_camera.output_size.h;
   float floor_centroid_frac = floor_centroid / (float)front_camera.output_size.h / 2.f;
 
+  //STUDENT ADDITIONS: The following prints are not necessary and could only slow down the nav process
   VERBOSE_PRINT("Color_count: %d  threshold: %d state: %d \n", color_count, color_count_threshold, navigation_state);
   VERBOSE_PRINT("Floor count: %d, threshold: %d\n", floor_count, floor_count_threshold);
   VERBOSE_PRINT("Floor centroid: %f\n", floor_centroid_frac);
 
-  // update our safe confidence using color threshold
+  //STUDENT ADDITIONS: update our safe confidence using color threshold by our vision
   if(leftorright != 0.0f){
     obstacle_free_confidence -= 2; // be more cautious with positive obstacle detections
     
   } else {
     obstacle_free_confidence++;  
   }
+  
   //VERBOSE_PRINT("left or right, obstacle_free confidence %f\n", leftorright, obstacle_free_confidence);
   VERBOSE_PRINT("left or right, %f\n",  leftorright);
-  VERBOSE_PRINT("oag color count %f\n", oag_color_count_frac);
-  // bound obstacle_free_confidence
+  // VERBOSE_PRINT("oag color count %f\n", oag_color_count_frac);
+  // //STUDENT ADDITIONS: bound obstacle_free_confidence so max 5 and min 0, is a preset number which is still quite okay (does not cause any problems)
   Bound(obstacle_free_confidence, 0, max_trajectory_confidence);
-
+  
+  //STUDENT ADDITIONS: changed speed, min is 0, max is 1,2
   float speed_sp = fminf(oag_max_speed, 0.5f * obstacle_free_confidence); //was 0.2
 
-  switch (navigation_state){
+  switch (navigation_state){ //STUDENT ADDITIONS: only SAFE, OBSTACLE_FOUND and SEARCH_FOR_SAFE_HEADING, reenter arena etc is covered in the vision part
     case SAFE:
       VERBOSE_PRINT("Safe %f\n", navigation_state);
       VERBOSE_PRINT("SAFE: %d, obstacle_free_confidence: %d\n", navigation_state, obstacle_free_confidence);
@@ -226,6 +231,8 @@ void mavguys_navigation_periodic(void)
 /*
  * Sets the variable 'incrementForAvoidance' randomly positive/negative
  */
+
+//STUDENT ADDITIONS: Both function stated below are used, the random one for the initialization and the determined one for the navigation through flight according to vision
 uint8_t chooseRandomIncrementAvoidance(void)
 {
   
