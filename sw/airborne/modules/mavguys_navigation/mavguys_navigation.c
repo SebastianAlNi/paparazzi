b41e06809381536d1e23f97827882380db01759c
Barry <barryvanleeuwen_95@hotmail.com>
2020-03-25 13:55:14 +0100

    Remove unnecessary code

--- sw/airborne/modules/mavguys_navigation/mavguys_navigation.c
+++ sw/airborne/modules/mavguys_navigation/mavguys_navigation.c
@@ -5,26 +5,15 @@
  *
  */
 /**
- * @file "modules/orange_avoider/orange_avoider_guided.c"
- * @author Kirk Scheper
- * This module is an example module for the course AE4317 Autonomous Flight of Micro Air Vehicles at the TU Delft.
- * This module is used in combination with a color filter (cv_detect_color_object) and the guided mode of the autopilot.
- * The avoidance strategy is to simply count the total number of orange pixels. When above a certain percentage threshold,
- * (given by color_count_frac) we assume that there is an obstacle and we turn.
- *
- * The color filter settings are set using the cv_detect_color_object. This module can run multiple filters simultaneously
- * so you have to define which filter to use with the ORANGE_AVOIDER_VISUAL_DETECTION_ID setting.
- * This module differs from the simpler orange_avoider.xml in that this is flown in guided mode. This flight mode is
- * less dependent on a global positioning estimate as witht the navigation mode. This module can be used with a simple
- * speed estimate rather than a global position.
- *
- * Here we also need to use our onboard sensors to stay inside of the cyberzoo and not collide with the nets. For this
- * we employ a simple color detector, similar to the 
- poles but for green to detect the floor. When the total amount
- * of green drops below a given threshold (given by floor_count_frac) we assume we are near the edge of the zoo and turn
- * around. The color detection is done by the cv_detect_color_object module, use the FLOOR_VISUAL_DETECTION_ID setting to
- * define which filter to use. 
- */
+ * @file "modules/mavguys_navigation/mavguys_navigation.c"
+ * @author Mavguys 
+ * this .c file is based on the original orange_avoider_guided navigation module.
+ * This document navigates the drone through the cyberzoo. This is done by information extracted from the vision part. The information is in principle
+ * just 0,1,2 which represents SAFE, LEFT and RIGHT. According to this command the drone actuators are controlled. A SAFE command will increase the confidence
+ * used during the navigation script and makes it fly straight a head and increase the speed to a limit. If the vision sends a LEFT or RIGHT signal, the navigation
+ * module will decrease the confidence and when confidence is 0, the drone will slow down, stop and turn left or right according to the command. 
+
+*/
 
 // STUDENT ADDITIONS: including necessary modules for the nav to run
 #include "modules/mavguys_navigation/mavguys_navigation.h"
@@ -52,42 +41,20 @@ enum navigation_state_t {
   SAFE,
   OBSTACLE_FOUND,
   SEARCH_FOR_SAFE_HEADING,
-  OUT_OF_BOUNDS,
-  REENTER_ARENA
 };
 
 // define settings
-float oag_color_count_frac = 0.18f;       // obstacle detection threshold as a fraction of total of image
-float oag_floor_count_frac = 0.05f;       // floor detection threshold as a fraction of total of image
 float oag_max_speed = 1.3f;               // max flight speed [m/s]
 float oag_heading_rate = RadOfDeg(30.f);  // heading change setpoint for avoidance [rad/s]
 float leftorright = 0.0f;                   //safe, links of rights
 
 // define and initialise global variables
 enum navigation_state_t navigation_state = SEARCH_FOR_SAFE_HEADING;   // current state in state machine
-int32_t color_count = 0;                // orange color count from color filter for obstacle detection
-int32_t floor_count = 0;                // green color count from color filter for floor detection
 int32_t floor_centroid = 0;             // floor detector centroid in y direction (along the horizon)
 float avoidance_heading_direction = 0;  // heading change direction for avoidance [rad/s]
 int16_t obstacle_free_confidence = 0;   // a measure of how certain we are that the way ahead if safe.
 
 const int16_t max_trajectory_confidence = 5;  // number of consecutive negative object detections to be sure we are obstacle free
-/*
-// This call back will be used to receive the color count from the orange detector
-#ifndef FLOOR_COLOR_DETECTOR_VISUAL_DETECTION_ID
-#error This module requires two color filters, as such you have to define FLOOR_COLOR_DETECTOR_VISUAL_DETECTION_ID to the orange filter
-#error Please define FLOOR_COLOR_DETECTOR_VISUAL_DETECTION_ID to be COLOR_OBJECT_DETECTION1_ID or COLOR_OBJECT_DETECTION2_ID in your airframe
-#endif
-static abi_event color_detection_ev;
-static void color_detection_cb(uint8_t __attribute__((unused)) sender_id,
-                               int16_t __attribute__((unused)) pixel_x, int16_t __attribute__((unused)) pixel_y,
-                               int16_t __attribute__((unused)) pixel_width, int16_t __attribute__((unused)) pixel_height,
-                               int32_t quality, int16_t __attribute__((unused)) extra)
-{
-  color_count = quality;
-}
-*/
-//int16_t leftorright = 1; //the value from 0,1,2 for safe, right and left from vision code
 
 // STUDENT ADDITIONS:Communication set-up between vision and navigation
 #ifndef FLOOR_VISUAL_DETECTION_ID
@@ -103,7 +70,6 @@ static void floor_detection_cb(uint8_t __attribute__((unused)) sender_id,
   leftorright = quality; //STUDENT ADDITIONS: command function (safe, right or left)
   floor_centroid = pixel_y;
 }
-
 /*
  * Initialisation function
  */
@@ -114,7 +80,6 @@ void mavguys_navigation_init(void) // STUDENT ADDITIONS: initialising the c-file
   chooseRandomIncrementAvoidance(); //STUDENT ADDITIONS: function is used in the beginning
 
   //STUDENT ADDITIONS: bind our vision program callbacks to receive the vision outputs
-  //AbiBindMsgVISUAL_DETECTION();//FLOOR_COLOR_DETECTOR_VISUAL_DETECTION_ID, &color_detection_ev, color_detection_cb);
   AbiBindMsgVISUAL_DETECTION(FLOOR_VISUAL_DETECTION_ID, &floor_detection_ev, floor_detection_cb);
 }
 
@@ -129,16 +94,6 @@ void mavguys_navigation_periodic(void)
     obstacle_free_confidence = 0;
     return;
   }
-  //
-  // compute current color thresholds
-  int32_t color_count_threshold = oag_color_count_frac * front_camera.output_size.w * front_camera.output_size.h;
-  int32_t floor_count_threshold = oag_floor_count_frac * front_camera.output_size.w * front_camera.output_size.h;
-  float floor_centroid_frac = floor_centroid / (float)front_camera.output_size.h / 2.f;
-
-  //STUDENT ADDITIONS: The following prints are not necessary and could only slow down the nav process
-  VERBOSE_PRINT("Color_count: %d  threshold: %d state: %d \n", color_count, color_count_threshold, navigation_state);
-  VERBOSE_PRINT("Floor count: %d, threshold: %d\n", floor_count, floor_count_threshold);
-  VERBOSE_PRINT("Floor centroid: %f\n", floor_centroid_frac);
 
   //STUDENT ADDITIONS: update our safe confidence using color threshold by our vision
   if(leftorright != 0.0f){
@@ -147,10 +102,8 @@ void mavguys_navigation_periodic(void)
   } else {
     obstacle_free_confidence++;  
   }
-  
-  //VERBOSE_PRINT("left or right, obstacle_free confidence %f\n", leftorright, obstacle_free_confidence);
+
   VERBOSE_PRINT("left or right, %f\n",  leftorright);
-  // VERBOSE_PRINT("oag color count %f\n", oag_color_count_frac);
   // //STUDENT ADDITIONS: bound obstacle_free_confidence so max 5 and min 0, is a preset number which is still quite okay (does not cause any problems)
   Bound(obstacle_free_confidence, 0, max_trajectory_confidence);
   
@@ -161,8 +114,6 @@ void mavguys_navigation_periodic(void)
     case SAFE:
       VERBOSE_PRINT("Safe %f\n", navigation_state);
       VERBOSE_PRINT("SAFE: %d, obstacle_free_confidence: %d\n", navigation_state, obstacle_free_confidence);
-      //if (floor_count < floor_count_threshold || fabsf(floor_centroid_frac) > 0.12){
-       // navigation_state = OUT_OF_BOUNDS;
        
       if (obstacle_free_confidence == 0){
         navigation_state = OBSTACLE_FOUND;
@@ -195,33 +146,7 @@ void mavguys_navigation_periodic(void)
         navigation_state = SAFE;
       }
        break;
-    // case OUT_OF_BOUNDS:
-    //   VERBOSE_PRINT("Out of bounds %f\n", navigation_state);
-    //   VERBOSE_PRINT("Out of bounds %d, obstacle_free_confidence: %d\n", navigation_state, obstacle_free_confidence);
-    //   // stop
-    //   guidance_h_set_guided_body_vel(0, 0);
-
-    //   // start turn back into arena
-    //   guidance_h_set_guided_heading_rate(avoidance_heading_direction * RadOfDeg(15));
-
-    //   navigation_state = REENTER_ARENA;
-
-    //   break;
-    // case REENTER_ARENA:
-    //   VERBOSE_PRINT("ARE YOU NOT ENTERTAINED? %f\n", navigation_state);
-    //   VERBOSE_PRINT("ARE YOU NOT ENTERTAINED?  %d, obstacle_free_confidence: %d\n", navigation_state, obstacle_free_confidence);
-    //   // force floor center to opposite side of turn to head back into arena
-    //   //if (floor_count >= floor_count_threshold && avoidance_heading_direction * floor_centroid_frac >= 0.f){
-    //     // return to heading mode
-    //     //guidance_h_set_guided_heading(stateGetNedToBodyEulers_f()->psi);
-
-    //     // reset safe counter
-    //     //obstacle_free_confidence = 0;
-
-    //     // ensure direction is safe before continuing
-    //     //navigation_state = SAFE;
-      
-    //   break;
+   
      default:
       break;
   }
@@ -234,10 +159,7 @@ void mavguys_navigation_periodic(void)
 
 //STUDENT ADDITIONS: Both function stated below are used, the random one for the initialization and the determined one for the navigation through flight according to vision
 uint8_t chooseRandomIncrementAvoidance(void)
-{
-  
-  //uit te commentarieren
-  
+{  
   if (rand() % 2 == 0) {
     avoidance_heading_direction = 1.f;
     VERBOSE_PRINT("Set avoidance increment to: %f\n", avoidance_heading_direction * oag_heading_rate);
